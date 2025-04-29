9ff61aa257b42e46efef29855d0b55de37e93c0a
wallace <w.calisto@yahoo.com.br>
2025-04-29 16:01:14 -0300

    chegaaa

--- html/welcome.php
+++ html/welcome.php
@@ -5,7 +5,26 @@
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
 </head>
+
+<style>
+      body {
+        background-color: #111;
+        color: #fff;
+        font-family: Arial, sans-serif;
+        text-align: center;
+        padding: 100px 20px;
+      }
+      a {
+        color: #66b134;
+        text-decoration: none;
+        font-weight: bold;
+      }
+      a:hover {
+        text-decoration: underline;
+      }
+    </style>
 <body>
-    <h1>sucesso</h1>
+      <h1>Usuário logado com sucesso!</h1>
+      <a href="../index.html">Voltar ao site</a>
 </body>
 </html>
